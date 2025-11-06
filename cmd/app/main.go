4b45411db328cfafbe2d5984edba3c49353cf3f5
electronic <electronic@MacBook-Air.local>
2025-11-06 14:35:30 +0300

    Minor update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	InitDB()
+	e.GET("/", getHandler)
+	e.POST("/", postHandler)
+	e.Start(":8080")
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func getHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!")
+}
+
+func postHandler(c echo.Context) error {
+	var u User
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid JSON"})
+	}
+	return c.JSON(http.StatusCreated, u)
 }
---
+++ internal/db/connect.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "host=localhost user=postgres pssword=pgpw02todo dbname=todo_db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+	DB = db
+	fmt.Println("Successful connect to database")
+}
